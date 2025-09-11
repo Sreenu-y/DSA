796042fcf428dff98adce106e7160d6db321e686
Sreenu-y <122997735+Sreenu-y@users.noreply.github.com>
2025-09-11 19:04:20 +0530

    Update minimumJumps.java

--- Greedy/minimumJumps.java
+++ Greedy/minimumJumps.java
@@ -1,1 +1,25 @@
-
+class Solution {
+    public int minJumps(int[] arr) {
+        int n = arr.length;
+        if(arr[0] == 0) return -1;
+        int max = arr[0], jumps = arr[0], ans = 1;
+        
+        for(int i = 1; i < n; i++) {
+            max--;
+            jumps--;
+            
+            max = Math.max(max, arr[i]);
+            
+            if(jumps == 0 && i != n - 1) {
+                if(max <= 0) {
+                    return -1;
+                }
+                jumps = max;
+                //max = 0;
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+}
